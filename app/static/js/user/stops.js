6f40376f441f30c7de5b41439dbe3af79b3fe536
David D. Boling <daviddboling@gmail.com>
2014-08-23 19:17:32 -0500

    Added map and directions to stops page, finished algorithm for waypoints

--- app/static/js/user/stops.js
+++ app/static/js/user/stops.js
@@ -8,14 +8,24 @@
   $(document).ready(function(){
     $('#add').click(addAdditionalStop);
 
-    //Convert waypoints to Google Map waypoints
-    var waypoints = waypoints.map(function(div){
-      var lat  = $('.coordinates').attr('data-lat'),
-      lng  = $('.coordinates').attr('data-lng');
-      return {location: new google.maps.LatLng(lat, lng)};
-    });
+    //Origin and Destination variables
+    var $coordinates = $('.coordinates'),
+           waypoints = [],
+              origin = new google.maps.LatLng($coordinates.attr('data-olat'), $coordinates.attr('data-olng')),
+         destination = new google.maps.LatLng($coordinates.attr('data-dlat'), $coordinates.attr('data-dlng'));
 
-    initialize(waypoints);
+
+
+    //Convert stops to waypoints for Google Maps
+    if(!waypoints.length){ 
+      waypoints = waypoints.map(function(div){
+        var lat  = $('.coordinates').attr('data-lat'),
+        lng  = $('.coordinates').attr('data-lng');
+        return {location: new google.maps.LatLng(lat, lng)};
+      });
+    }
+
+    initialize(origin, destination, waypoints);
 
 
   });
@@ -24,15 +34,12 @@
 
   }
 
-  function initialize(wps){
-    var origin        = wps.shift(),
-        destination   = wps.pop(),
-    directionsService = new google.maps.DirectionsService(),
-    directionsDisplay = new google.maps.DirectionsRenderer(),
-               styles = [{'featureType':'water','stylers':[{'color':'#021019'}]},{'featureType':'landscape','stylers':[{'color':'#08304b'}]},{'featureType':'poi','elementType':'geometry','stylers':[{'color':'#0c4152'},{'lightness':5}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'color':'#000000'}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'color':'#0b434f'},{'lightness':25}]},{'featureType':'road.arterial','elementType':'geometry.fill','stylers':[{'color':'#000000'}]},{'featureType':'road.arterial','elementType':'geometry.stroke','stylers':[{'color':'#0b3d51'},{'lightness':16}]},{'featureType':'road.local','elementType':'geometry','stylers':[{'color':'#000000'}]},{'elementType':'labels.text.fill','stylers':[{'color':'#ffffff'}]},{'elementType':'labels.text.stroke','stylers':[{'color':'#000000'},{'lightness':13}]},{'featureType':'transit','stylers':[{'color':'#146474'}]},{'featureType':'administrative','elementType':'geometry.fill','stylers':[{'color':'#000000'}]},{'featureType':'administrative','elementType':'geometry.stroke','stylers':[{'color':'#144b53'},{'lightness':14},{'weight':1.4}]}],
+  function initialize(origin, destination, stops){
+
+    var styles = [{'featureType':'water','stylers':[{'color':'#021019'}]},{'featureType':'landscape','stylers':[{'color':'#08304b'}]},{'featureType':'poi','elementType':'geometry','stylers':[{'color':'#0c4152'},{'lightness':5}]},{'featureType':'road.highway','elementType':'geometry.fill','stylers':[{'color':'#000000'}]},{'featureType':'road.highway','elementType':'geometry.stroke','stylers':[{'color':'#0b434f'},{'lightness':25}]},{'featureType':'road.arterial','elementType':'geometry.fill','stylers':[{'color':'#000000'}]},{'featureType':'road.arterial','elementType':'geometry.stroke','stylers':[{'color':'#0b3d51'},{'lightness':16}]},{'featureType':'road.local','elementType':'geometry','stylers':[{'color':'#000000'}]},{'elementType':'labels.text.fill','stylers':[{'color':'#ffffff'}]},{'elementType':'labels.text.stroke','stylers':[{'color':'#000000'},{'lightness':13}]},{'featureType':'transit','stylers':[{'color':'#146474'}]},{'featureType':'administrative','elementType':'geometry.fill','stylers':[{'color':'#000000'}]},{'featureType':'administrative','elementType':'geometry.stroke','stylers':[{'color':'#144b53'},{'lightness':14},{'weight':1.4}]}],
     options = {
       zoom: 2,
-      center: wps[0].location,
+      center: origin,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       styles: styles
     };
@@ -41,19 +48,22 @@
     map = new google.maps.Map(document.getElementById('map'), options);
 
     //Render options specific to directionsDisplay. E.G. the lines on the map
-    var rendererOptions = {map: map};
+    var rendererOptions = {map: map},
     directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
 
     //Format the request to directions service
     var request = {
-      origin: origin.location,
-      destination: destination.location,
-      waypoints: wps,
+      origin: origin,
+      destination: destination,
+      waypoints: stops,
       travelMode: google.maps.DirectionsTravelMode.DRIVING
     };
 
+    //Display driving directions
+    var directionsService = new google.maps.DirectionsService();
+    directionsDisplay.setPanel(document.getElementById('directions'));
+    
     //Sends our request to directionsService where it will tell us if we're crazy or not
-    directionsService = new google.maps.DirectionsService();
     directionsService.route(request, function(response, status) {
           if (status === google.maps.DirectionsStatus.OK) {
             directionsDisplay.setDirections(response);
